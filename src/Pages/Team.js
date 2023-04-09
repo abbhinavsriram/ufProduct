993f26a47fc01529405ca36436a94b857ef8e49b
wuseokjung <wuseok.jung@gmail.com>
2023-04-08 22:16:05 -0400

    Team page update

--- src/Pages/Team.js
+++ src/Pages/Team.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CustomNavbar from '../components/CustomNavbar';
 import Breadcrumb from '../components/Breadcrumb';
 import Teams from '../components/Team/Team';
+import Sservice from '../components/Service/Sservice/Sservice';
 import FooterTwo from '../components/Footer/FooterTwo';
 import FooterData from '../components/Footer/FooterData';
 import Partner from '../components/Partner';
@@ -20,6 +21,7 @@ const Team = () => {
 				Ptitle="Our Team"
 				Pdescription="Why I say old chap that is spiffing off his nut arse pear shaped plastered Jeffrey bodge barney some dodgy.!!"
 			/>
+			<Sservice />
 			<Teams />
 			<Partner pClass="partner_logo_area_five bg_color" />
 			<FooterTwo FooterData={FooterData} />
